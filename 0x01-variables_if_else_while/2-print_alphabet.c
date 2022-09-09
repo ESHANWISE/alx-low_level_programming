e87912709e8a9d33dc88e3d6e778ea16f9352725
eshanwise <eshanwise@gmail.com>
2022-09-09 13:45:30 -0700

    &

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,30 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
-
-
 /**
-
- * main - lowercase alphabets
-
- *
-
- * Return: Always (Success)
-
- */
-
+* main - print if the number is postive, zero, or negative
+*
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
+* Return: 0
+*/
 int main(void)
-
 {
+int n;
+int l;
 
-        char c;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+l = n % 10;
 
-
-        for (c = 'a'; c <= 'z'; c++)
-
-                putchar(c);
-
-
-        putchar('\n');
-
-        return (0);
-
+if (l > 5)
+{
+	printf("Last digit of %d is %d and is greater than 5\n", n, l);
+}
+else if (l == 0)
+{
+	printf("Last digit of %d is %d and is 0\n", n, l);
+}
+else
+{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+}
+return (0);
 }
