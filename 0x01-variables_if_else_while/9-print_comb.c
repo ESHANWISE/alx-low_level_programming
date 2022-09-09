cb65c3c803ac22a1df021306a22d1bb5030fc024
eshanwise <eshanwise@gmail.com>
2022-09-09 15:22:51 -0700

    &

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,28 +9,27 @@
 int main(void)
 
 {
-        int c;
+int c;
 
-        for (c = '0'; c <= '9'; c++)
+for (c = '0'; c <= '9'; c++)
 
-        {
+{
 
                 putchar(c);
 
                 if (c != '9')
 
-                {
+{
 
                         putchar(',');
 
                         putchar(' ');
 
-                }
+}
 
-        }
+}
 
         putchar('\n');
-
-        return (0);
+return (0);
 
 }
