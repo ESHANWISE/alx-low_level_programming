cabd8e226b0467efbe66af80195eee95f5ea5f43
eshanwise <eshanwise@gmail.com>
2022-09-09 17:50:36 -0700

    &

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,31 +5,18 @@
   *
   * Return: Always (Success);
   */
-
 int main(void)
-
 {
 int c;
-
 for (c = '0'; c <= '9'; c++)
-
 {
-
-                putchar(c);
-
-                if (c != '9')
-
+putchar(c);
+if (c != '9')
 {
-
-                        putchar(',');
-
-                        putchar(' ');
-
+putchar(',');
+putchar(' ');
 }
-
 }
-
-        putchar('\n');
+putchar('\n');
 return (0);
-
 }
