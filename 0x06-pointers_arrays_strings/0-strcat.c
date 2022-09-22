9bbfa4d2d8e24cfb432a5959630dda9df98f5e12
eshanwise <eshanwise@gmail.com>
2022-09-22 05:21:57 -0700

    &

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,21 +1,30 @@
-#include"main.h"
+#include "main.h"                                                                                                                  
 
-/**
- * strcat-concatenates the string pointed to by @src to
- * the end of the string pointed to by @dest
- * @dest: string that will be appended
- * @src: String to be concatenated upon
- * return: return pointer to @dest
- */
+#include <unistd.h>                                                                                                                    
 
-char*_strcat(char*dest, char*src)
-{
-int index =0, dest_len=0;
-      while(dest[index++];
-	
-      for(index=0; src[index]; index++)
-               dest[dest_len++]=src[index];
+                                                                                                                                       
+
+/**                                                                                                                                    
+
+ * _putchar - writes the character c to stdout                                                                                          
+
+ * @c: The character to print                                                                                                          
+
+ *                                                                                                                                      
+
+ * Return: On success 1.                                                                                                                
+
+ * On error, -1 is returned, and errno is set appropriately.                                                                            
+
+ */                                                                                                                                    
+
+int _putchar(char c)                                                                                                                    
+
+{                                                                                                                                      
+
+        return (write(1, &c, 1));                                                                                                      
+
+}                              
 
 
-    return (dest);
-}
+
