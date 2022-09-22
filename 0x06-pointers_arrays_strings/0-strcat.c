72310f529da1b1b652b74b3820f6e28880fe623a
eshanwise <eshanwise@gmail.com>
2022-09-22 12:40:21 -0700

    &

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,30 +1,24 @@
-#include "main.h"                                                                                                                  
+#include "main.h"
 
-#include <unistd.h>                                                                                                                    
+/**
+ *_strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ *@dest: String that will be appended
+ *@src: String to be concatenated upon
+ *
+ * Return: returns poiner to @dest
+ */
 
-                                                                                                                                       
+char *_strcat(char *dest, char *src)
+{
 
-/**                                                                                                                                    
+	int index = 0, dest_len = 0;
 
- * _putchar - writes the character c to stdout                                                                                          
+	while (dest[index++])
+		dest_len++;
 
- * @c: The character to print                                                                                                          
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
- *                                                                                                                                      
-
- * Return: On success 1.                                                                                                                
-
- * On error, -1 is returned, and errno is set appropriately.                                                                            
-
- */                                                                                                                                    
-
-int _putchar(char c)                                                                                                                    
-
-{                                                                                                                                      
-
-        return (write(1, &c, 1));                                                                                                      
-
-}                              
-
-
-
+	return (dest);
+}
