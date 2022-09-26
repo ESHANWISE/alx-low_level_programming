c33c2cb968b85058d3dca62757b8237d59795883
eshanwise <eshanwise@gmail.com>
2022-09-26 13:46:30 -0700

    &

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,5 +7,6 @@
  * @src: source to copy from
  * @n: number of bytes to write                                  *                                                               * Return: pointer to dest                                       */                                                             char *_memcpy(char *dest, char *src, int n)                     {                                                                       char *ptr = dest;                                                                                                               while (n--)
                 *ptr++ = *src++;                                
-        return (dest);
+
+	return (dest);
 }
