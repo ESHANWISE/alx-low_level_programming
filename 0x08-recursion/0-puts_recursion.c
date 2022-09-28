a67d9cdc95874cfe588c0fd1e75d95346a873678
eshanwise <eshanwise@gmail.com>
2022-09-28 13:09:49 -0700

    v

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,9 +6,9 @@
  *
  * return: void
  * */
-void _puts_recursiion(char *s)
+void _puts_recursion(char *s)
 {
-	if (*s == '\0');
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -27,7 +27,8 @@ void check(char *s, int start, int end, int *flag)
 	{
 		if (s[start] == s[end])
 			*flag *= 1;
-		else *flag *= 0;
+		else
+		       	*flag *= 0;
 		check(s, start + 1, end - 1, flag);
 	}
 }
